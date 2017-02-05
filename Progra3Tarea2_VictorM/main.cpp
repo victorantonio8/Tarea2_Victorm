cd3bdda7399b08dd242a91cc929475696ee3d194
victorantonio8 <victormendez@unitec.edu>
2017-02-04 22:05:35 -0600

    Add files via upload

---
+++ Progra3Tarea2_VictorM/main.cpp
@@ -0,0 +1,77 @@
+#include "Evaluador.h"
+#include <iostream>
+using namespace std;
+
+//Desrefencia ptr (dado) y devuelve su valor
+int obtenerValor(int *ptr)
+{
+
+    return *ptr;
+}
+
+//Desreferencia a (dado) y b (dado) y devuelve la suma de ambos
+int sumar(int* a, int* b)
+{
+    return *a+*b;
+}
+
+//Devuelve true si a (dado) y b (dado) apuntan al mismo espacio de memoria
+bool compararApuntadores(string *a, string *b)
+{
+    if (a==b)
+
+    return true;
+}
+
+//Desrefencia a (dado) y b (dado), devuelve true si ambos valores son iguales de lo contrario devuelve false
+bool comparar(string *a, string *b)
+{
+    if (*a==*b)
+    {
+        return true;
+    }
+    else{
+
+
+    return false;
+}
+
+//Desreferencia a (dado) y devuelve su primera letra
+char getPrimeraLetra(string* a)
+{
+    string c;
+    c=*a;
+    return c[0];
+}
+
+//Desreferencia a (dado) y devuelve true si es palindroma de lo contrario devuelve false
+//Definicion de palindromo: http://es.wikipedia.org/wiki/Pal%C3%ADndromo
+//Ejemplos de palindromos: Ana, arenera, arepera, anilina, ananá, Malayalam, Neuquén, Oruro, oso, radar, reconocer, rotor, salas, seres, somos, sometemos
+bool esPalindromo(string* a)
+{
+    string palindromo = *a;
+    if(palindromo == string(palindromo.rbegin(),palindromo.rend()))
+    {
+        return true;
+    }
+    else{
+            return false;
+    }
+
+}
+
+//Desreferencia base (dado) y exponente (dado) y devuelve la base elevado al exponente
+int getExponente(int* base, int* exponente)
+{
+    int b = *base;
+    int e =*exponente;
+    int respuesta = pow(b,e);
+    return respuesta;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
